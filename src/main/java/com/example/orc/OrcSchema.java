ff0dbb548b25502583de5df0f3ba6ba2ce82ae25
yuokada <callistoiv+git@gmail.com>
2017-12-28 22:37:07 +0900

    reformat

--- src/main/java/com/example/orc/OrcSchema.java
+++ src/main/java/com/example/orc/OrcSchema.java
@@ -9,8 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
-import org.apache.hadoop.hive.ql.io.orc.OrcFileKeyWrapper;
 import org.apache.orc.TypeDescription;
 
 /**
@@ -18,44 +16,44 @@ import org.apache.orc.TypeDescription;
  */
 public class OrcSchema {
 
-  public static TypeDescription newSchema(Path json_path) throws IOException {
-    Map<String, Object> map = readJsonFile(json_path);
-    System.out.println(map);
-    for (String key:map.keySet()){
-      if (key.equals("properties")) {
-        Object properties = map.get(key);
-        Map<String, Map<String, Object>> mprop =
-            (Map<String, Map<String, Object>>) properties;
-        for (Map.Entry<String, Map<String, Object>> es: mprop.entrySet()){
-          if (es.getValue().size() != 2) {
-            Map<String, Object> v = es.getValue();
-            v.get("type"); // array or object
-            System.out.println("Oh... " + es.getKey() + "!");
-          }
-        }
-      }
+    public static TypeDescription newSchema(Path json_path) throws IOException {
+        Map<String, Object> map = readJsonFile(json_path);
+        System.out.println(map);
+        for (String key : map.keySet()) {
+            if (key.equals("properties")) {
+                Object properties = map.get(key);
+                Map<String, Map<String, Object>> mprop =
+                    (Map<String, Map<String, Object>>) properties;
+                for (Map.Entry<String, Map<String, Object>> es : mprop.entrySet()) {
+                    if (es.getValue().size() != 2) {
+                        Map<String, Object> v = es.getValue();
+                        v.get("type"); // array or object
+                        System.out.println("Oh... " + es.getKey() + "!");
+                    }
+                }
+            }
 //      OrcFileKeyWrapper
+        }
+
+        TypeDescription schema = TypeDescription.createStruct();
+        schema.addField("field1", TypeDescription.createInt())
+            .addField("field2", TypeDescription.createString())
+            .addField("field3", TypeDescription.createString())
+            .addField("sfield1", TypeDescription.createStruct());
+        return schema;
     }
 
-    TypeDescription schema = TypeDescription.createStruct();
-    schema.addField("field1", TypeDescription.createInt())
-        .addField("field2", TypeDescription.createString())
-        .addField("field3", TypeDescription.createString())
-        .addField("sfield1", TypeDescription.createStruct());
-    return schema;
-  }
-
-  private static Map<String, Object> readJsonFile(Path json_path) throws IOException {
-    BufferedReader br = Files.newBufferedReader(json_path, StandardCharsets.UTF_8);
-    // JSON -> Map
-    Map<String, Object> map = new LinkedHashMap<>();
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      map = mapper.readValue(br, new TypeReference<LinkedHashMap<String, Object>>() {
-      });
-    } catch (IOException e) {
-      e.printStackTrace();
+    private static Map<String, Object> readJsonFile(Path json_path) throws IOException {
+        BufferedReader br = Files.newBufferedReader(json_path, StandardCharsets.UTF_8);
+        // JSON -> Map
+        Map<String, Object> map = new LinkedHashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(br, new TypeReference<LinkedHashMap<String, Object>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
     }
-    return map;
-  }
 }
--- src/main/java/com/example/orc/OrcWriter.java
+++ src/main/java/com/example/orc/OrcWriter.java
@@ -2,53 +2,54 @@
 // https://gist.github.com/omalley
 package com.example.orc;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.function.LongConsumer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hive.ql.exec.vector.*;
+import org.apache.hadoop.hive.ql.exec.vector.BytesColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.StructColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.orc.CompressionKind;
-import org.apache.orc.TypeDescription;
 import org.apache.orc.OrcFile;
+import org.apache.orc.TypeDescription;
 import org.apache.orc.Writer;
 
 //import org.apache.hadoop.hive.ql.io.orc.OrcFile;
 
-import java.io.IOException;
-
 public class OrcWriter {
 
-  public static void main(String[] args) throws IOException,
-                                                InterruptedException {
+    public static void main(String[] args) throws IOException,
+        InterruptedException {
 
-    String path = "/tmp/file2.orc";
-    TypeDescription schema = OrcSchema.newSchema(Paths.get("etc/sample.json"));
-    Configuration conf = new Configuration();
+        String path = "/tmp/file2.orc";
+        TypeDescription schema = OrcSchema.newSchema(Paths.get("etc/sample.json"));
+        Configuration conf = new Configuration();
 
-    try{
-        Writer writer = OrcFile.createWriter(new Path(path),
-        OrcFile.writerOptions(conf)
-          .setSchema(schema)
-          .stripeSize(100000)
-          .bufferSize(10000)
-          .compress(CompressionKind.ZLIB)
-          .version(OrcFile.Version.V_0_12));
-      VectorizedRowBatch batch = schema.createRowBatch();
-      batch.size = 1;
-      ((LongColumnVector) batch.cols[0]).vector[0] = 1;
-      ((BytesColumnVector) batch.cols[1]).setVal(0, "hello".getBytes());
-      ((BytesColumnVector) batch.cols[2]).setVal(0, "orcFile".getBytes());
-      StructColumnVector x = ((StructColumnVector) batch.cols[4]);
+        try {
+            Writer writer = OrcFile.createWriter(new Path(path),
+                OrcFile.writerOptions(conf)
+                    .setSchema(schema)
+                    .stripeSize(100000)
+                    .bufferSize(10000)
+                    .compress(CompressionKind.ZLIB)
+                    .version(OrcFile.Version.V_0_12));
+            VectorizedRowBatch batch = schema.createRowBatch();
+            batch.size = 1;
+            ((LongColumnVector) batch.cols[0]).vector[0] = 1;
+            ((BytesColumnVector) batch.cols[1]).setVal(0, "hello".getBytes());
+            ((BytesColumnVector) batch.cols[2]).setVal(0, "orcFile".getBytes());
+            StructColumnVector x = ((StructColumnVector) batch.cols[4]);
 
-      writer.addRowBatch(batch);
-      writer.close();
+            writer.addRowBatch(batch);
+            writer.close();
 
-      // cleanup
-      Files.delete(Paths.get(path));
-    }catch (Exception e){
-      e.printStackTrace();
-      System.exit(1);
+            // cleanup
+            Files.delete(Paths.get(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
-  }
 }
